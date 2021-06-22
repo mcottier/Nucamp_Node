2622d0a7cd14582970fcc8ad3c69589e986ccfa0
Maria <cottiermaria@myocu.oak.edu>
2021-06-21 19:31:23 -0500

    Routing Finished

--- 5-NodeJS-Express-MongoDB/node-express/routes/partnerRouter.js
+++ 5-NodeJS-Express-MongoDB/node-express/routes/partnerRouter.js
@@ -2,53 +2,61 @@ const express = require("express");
 const partnerRouter = express.Router();
 
 partnerRouter
-  .route("/partners")
+  .route("/")
   .all((req, res, next) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     next();
-  })
+  }) //do not put semicolons between these, they are chained thank you
+
   .get((req, res) => {
     res.end("Will send all the partners to you");
   })
+
   .post((req, res) => {
     res.end(
       `Will add the partners: ${req.body.name} with description: ${req.body.description}`
     );
   })
+
   .put((req, res) => {
     res.statusCode = 403;
-    res.end("PUT partner not supported on /partner");
+    res.end("PUT operation not supported on /partners");
   })
+
   .delete((req, res) => {
     res.end("Deleting all partners");
   });
-partnerRouter.route("/partners/:partnerId").all((req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  next()
-    .get((req, res) => {
-      res.end(
-        `Will send details of the partners: ${req.params.partnerId} to you`
-      );
-    })
 
-    .post((req, res) => {
-      res.statusCode = 403;
-      res.end(
-        `POST operation not supported on /partners/${req.params.partnerId}`
-      );
-    })
+partnerRouter
+  .route("/:partnerId")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  }) //more chaining do not put semicolons until delete
 
-    .post((req, res) => {
-      res.write(`Updating the partners: ${req.params.partnerId}\n`);
-      res.end(`Will update the partners: ${req.body.name}
+  .get((req, res) => {
+    res.end(
+      `Will send details of the partners: ${req.params.partnerId} to you`
+    );
+  })
+
+  .post((req, res) => {
+    res.statusCode = 403;
+    res.end(
+      `POST operation not supported on /partners/${req.params.partnerId}`
+    );
+  })
+
+  .put((req, res) => {
+    res.write(`Updating the partners: ${req.params.partnerId}\n`);
+    res.end(`Will update the partners: ${req.body.name}
         with description: ${req.body.description}`);
-    })
+  })
 
-    .delete("/partners/:partnerId", (req, res) => {
-      res.end(`Deleting partners: ${req.params.partnerId}`);
-    });
-});
+  .delete((req, res) => {
+    res.end(`Deleting partners: ${req.params.partnerId}`);
+  });
 
 module.exports = partnerRouter;
--- 5-NodeJS-Express-MongoDB/node-express/routes/promotionRouter.js
+++ 5-NodeJS-Express-MongoDB/node-express/routes/promotionRouter.js
@@ -2,53 +2,61 @@ const express = require("express");
 const promotionRouter = express.Router();
 
 promotionRouter
-  .route("/promotions")
+  .route("/")
   .all((req, res, next) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     next();
-  })
+  }) //do not put semicolons between these, they are chained thank you
+
   .get((req, res) => {
     res.end("Will send all the promotions to you");
   })
+
   .post((req, res) => {
     res.end(
       `Will add the promotions: ${req.body.name} with description: ${req.body.description}`
     );
   })
+
   .put((req, res) => {
     res.statusCode = 403;
-    res.end("PUT operation not supported on /promotion");
+    res.end("PUT operation not supported on /promotions");
   })
+
   .delete((req, res) => {
     res.end("Deleting all promotions");
   });
-promotionRouter.route("/promotions/:promotionId").all((req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  next()
-    .get((req, res) => {
-      res.end(
-        `Will send details of the promotions: ${req.params.promotionId} to you`
-      );
-    })
 
-    .post((req, res) => {
-      res.statusCode = 403;
-      res.end(
-        `POST operation not supported on /promotions/${req.params.promotionId}`
-      );
-    })
+promotionRouter
+  .route("/:promotionId")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  }) //more chaining do not put semicolons until delete
 
-    .post((req, res) => {
-      res.write(`Updating the promotions: ${req.params.promotionId}\n`);
-      res.end(`Will update the promotions: ${req.body.name}
+  .get((req, res) => {
+    res.end(
+      `Will send details of the promotions: ${req.params.promotionId} to you`
+    );
+  })
+
+  .post((req, res) => {
+    res.statusCode = 403;
+    res.end(
+      `POST operation not supported on /promotions/${req.params.promotionId}`
+    );
+  })
+
+  .put((req, res) => {
+    res.write(`Updating the promotions: ${req.params.promotionId}\n`);
+    res.end(`Will update the promotions: ${req.body.name}
         with description: ${req.body.description}`);
-    })
+  })
 
-    .delete("/promotions/:promotionId", (req, res) => {
-      res.end(`Deleting promotions: ${req.params.partnerId}`);
-    });
-});
+  .delete((req, res) => {
+    res.end(`Deleting promotions: ${req.params.promotionId}`);
+  });
 
 module.exports = promotionRouter;
